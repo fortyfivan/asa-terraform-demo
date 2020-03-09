9ec57b93c0d7b313411b5ed01ffbf1ff97c0c3c0
fortyfivan <fortyfivan@gmail.com>
2020-03-08 19:44:09 -0700

    Fix errors

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "oktaasa_project" "asa_project" {
 
 resource "oktaasa_enrollment_token" "enrollment_token" {
   project_name = oktaasa_project.asa_project.project_name
+  description  = "ASA enrollment token for project"
 }
 
 resource "oktaasa_create_group" "group_name" {
@@ -12,7 +13,7 @@ resource "oktaasa_create_group" "group_name" {
 
 resource "oktaasa_assign_group" "group-assignment" {
   project_name  = oktaasa_project.asa_project.project_name
-  group_name    = oktaasa_project.asa_project.group_name
+  group_name    = oktaasa_create_group.group_name.name
   server_access = true
   server_admin  = true
 }
@@ -21,15 +22,15 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags {
-    "Environment" = var.environment_tag
+  tags = {
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
-  tags {
-    "Environment" = var.environment_tag
+  tags = {
+    Environment = var.environment_tag
   }
 }
 
@@ -38,8 +39,8 @@ resource "aws_subnet" "subnet" {
   cidr_block              = var.cidr_subnet
   map_public_ip_on_launch = "true"
   availability_zone       = var.availability_zone
-  tags {
-    "Environment" = var.environment_tag
+  tags = {
+    Environment = var.environment_tag
   }
 }
 
@@ -49,8 +50,8 @@ resource "aws_route_table" "route_table" {
       cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.igw.id
   }
-  tags {
-    "Environment" = var.environment_tag
+  tags = {
+    Environment = var.environment_tag
   }
 }
 
@@ -74,23 +75,19 @@ resource "aws_security_group" "security_group" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags {
-    "Environment" = var.environment_tag
+  tags = {
+    Environment = var.environment_tag
   }
-}
-
-data "template_file" "sftd-userdata" {
-  templatefile("sftd-userdata.sh", { sftd_version = var.sftd_version, enrollment_token = oktaasa_enrollment_token.enrollment_token})
 }
 
 resource "aws_instance" "ec2_instance" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.subnet_public.id
-  vpc_security_group_ids = aws_security_group.sg_ssh.id
-  user_data              = data.template_file.sftd-userdata.rendered
-  tags {
-    "Environment" = var.environment_tag
+  subnet_id              = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  user_data              = templatefile("sftd-userdata.sh", { sftd_version = var.sftd_version, enrollment_token = oktaasa_enrollment_token.enrollment_token.token_value})
+  tags = {
+    Environment = var.environment_tag
   }
 }
 
